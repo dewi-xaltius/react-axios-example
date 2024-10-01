4df422319435463fe52b60d4a9db655145c115d0
dewi-xaltius <smdewi@xaltius.tech>
2024-10-01 10:16:58 +0800

    add ExampleComponent axios GET from jsonplaceholder

---
+++ src/components/ExampleComponent.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ExampleComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Example Component</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ExampleComponent;
